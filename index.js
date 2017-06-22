6aa6aa0b12588d651a81499356bf2b57f12d40c8
Renan Oliveira <renan.oliveira@mobdiq.com.br>
2017-06-22 15:58:13 -0300

    Estudos de Herança

--- index.js
+++ index.js
@@ -19,10 +19,37 @@ class Message{
     }
 }
 
+class ImageMessage extends Message{
+    constructor(text = '', created = Date.now(),
+                url = '', thumbnail = ''){
+       super(text, created);
+       this.url = url;
+       this.thumbnail = thumbnail;
+    }
+    toString(){
+        return `Photo${super.toString()}, url: ${this.url}, thumbnail: ${this.thumbnail}`;
+    }
+}
+
+var text = 'Some Text';
+var created = Date.now();
+
+var duckTypeMessage = {
+    text,
+    created
+};
+
 var emptyMessage = new Message();
 var textMessage = new Message('Yesterday Message', Date.now() - 86400);
+var photoMessage = new ImageMessage();
 
 console.log(emptyMessage);
 console.log(textMessage);
 console.log(String(emptyMessage));
 console.log(String(textMessage));
+console.log(String(photoMessage));
+console.log(duckTypeMessage);
+
+console.log(emptyMessage instanceof Message);
+console.log(textMessage instanceof Message);
+console.log(photoMessage instanceof Message);
